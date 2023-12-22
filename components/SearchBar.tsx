fef3fc071d04eeb7277f238f53a594236d86aac0
prernasyan <prernasyan22@gmail.com>
2023-12-22 11:04:40 +0530

    first commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import React from "react";
+import { SearchManufacturer } from "./";
+import Image from "next/image";
+
+const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+  const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image
+        src="/magnifying-glass.svg"
+        alt="magnifying glass"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
